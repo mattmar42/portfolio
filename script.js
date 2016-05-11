aada98a7d9da364d51c3079efe7385f5629f3754
Matt <matthewmaring42@gmail.com>
2016-05-11 16:46:26 -0400

    linked nav elements, added smooth scrolling

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
-	$('h1').fadeIn(500);
-	$('p').fadeIn(2000);
-	$('li').fadeIn(2000);
 
+	$('h1').fadeIn(1000);
+	$('p').fadeIn(2200);
+	$('li').fadeIn(2200);
+
+	$('a[href^="#"]').on('click', function(event){
+
+    var target = $($(this).attr('href'));
+
+    if (target.length){
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: target.offset().top - 75
+        }, 1000);
+    }
+
+});
+	
 });
